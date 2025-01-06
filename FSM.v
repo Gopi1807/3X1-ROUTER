08148faf704ca6e1e4f3939836e82a296217a530
Gopi chand Ananthu <167249902+Gopi1807@users.noreply.github.com>
2025-01-06 16:30:18 -0500

    FSM.v

--- FSM.v
+++ FSM.v
@@ -1,112 +1,146 @@
 module fsm(
-input clk,resetn,pkt_valid,
-input parity_done,
-input soft_reset_0,soft_reset_1,soft_reset_2,
-input fifo_full,
-input low_pkt_valid,
-input fifo_empty_0,fifo_empty_1,fifo_empty_2,
-input  data_in,
-output detect_add,
-output Id_state,
-output laf_state,
-output full_state,
-output write_enb_reg,
-output rst_int_reg,
-output lfd_state);
+    input clk,
+    input resetn,
+    input pkt_valid,
+    input parity_done,
+    input soft_reset_0, soft_reset_1, soft_reset_2,
+    input fifo_full,
+    input low_pkt_valid,
+    input fifo_empty_0, fifo_empty_1, fifo_empty_2,
+    input [1:0] data_in, 
 
+    output reg detect_add,
+    output reg Id_state,
+    output reg laf_state,
+    output reg full_state,
+    output reg write_enb_reg,
+    output reg rst_int_reg,
+    output reg lfd_state,
+    output reg busy
+);
 
-parameter DECODE_ADDRESS = 3'b000,
-          WAIT_TILL_EMPTY = 3'b001,
-          LOAD_FIRST_DATA = 3'b010,
-          LOAD_DATA = 3'b011,
-          FIF0_FULL_STATE = 3'b100,
-          LOAD_AFTER_FULL = 3'b101,
-          LOAD_PARITY = 3'b110,
-          CHECK_PARITY_ERROR = 3'b111;
+    
+    parameter IDLE              = 3'b000,
+              DECODE_ADDRESS    = 3'b001,
+              WAIT_TILL_EMPTY   = 3'b010,
+              LOAD_FIRST_DATA   = 3'b011,
+              LOAD_DATA         = 3'b100,
+              FIFO_FULL_STATE   = 3'b101,
+              LOAD_AFTER_FULL   = 3'b110,
+              LOAD_PARITY       = 3'b111,
+              CHECK_PARITY_ERROR = 3'b1000; 
 
+    reg [3:0] state, next_state;
 
-reg [1:0] state,next_state;
-
-always@(posedge clock or negedge resetn)
-if(!rst_n)
+    
+    always @(posedge clk or negedge resetn) begin
+        if (!resetn) begin
             state <= IDLE;
-        else
+        end else if (soft_reset_0 || soft_reset_1 || soft_reset_2) begin
+            state <= DECODE_ADDRESS; 
+        end else begin
             state <= next_state;
-    end 
- 
-always @(state or pkt_valid or data_in or fifo_empty_0 or fifo_empty_1 or fifo_empty_2) begin
-next_state = DECODE_ADDRESS;
-    if (state == DECODE_ADDRESS) begin
-        if ((pkt_valid & (data_in[1:0] == 2'b00) & fifo_empty_0) |
-            (pkt_valid & (data_in[1:0] == 2'b01) & fifo_empty_1) |
-            (pkt_valid & (data_in[1:0] == 2'b10) & fifo_empty_2)) begin
-       
-            next_state = LOAD_FIRST_DATA;
-        end
-        else if ((pkt_valid & (data_in[1:0] == 2'b00) & !fifo_empty_0) |
-                 (pkt_valid & (data_in[1:0] == 2'b01) & !fifo_empty_1) |
-                 (pkt_valid & (data_in[1:0] == 2'b10) & !fifo_empty_2)) 
-        begin
-            next_state = WAIT_TILL_EMPTY;
         end
     end
- 
-    if (state == WAIT_TILL_EMPTY ) begin
+
+    
+    always @(*) begin
         
-         if ((fifo_empty_0 && (addr == 0)) ||
-             (fifo_empty_1 && (addr == 1)) ||
-             (fifo_empty_2 && (addr == 2)) begin
-                       
-            next_state = LOAD_FIRST_DATA;
-        end 
-      else 
-         begin 
-          next_state = WAIT_TILL_EMPTY;
-    end
-   end
+        next_state = state;
+        detect_add = 0;
+        Id_state = 0;
+        laf_state = 0;
+        full_state = 0;
+        write_enb_reg = 0;
+        rst_int_reg = 0;
+        lfd_state = 0;
+        busy = 0;
 
-   if (state == LOAD_FIRST_DATA) begin
-      next_state = LOAD_DATA;
-    end
- 
-   if (state == LOAD_DATA ) begin
-       next_state = LOAD_DATA;
-       if(fifo_full) begin
-          next_state = FIFO_FULL_STATE;
+        case (state)
+            IDLE: begin
+                if (pkt_valid) begin
+                    next_state = DECODE_ADDRESS;
+                end
             end
-       if(!fifo_full && !pkt_valid) begin
-          next_state = LOAD_PARITY ;
-             end
-   end
-         if(state==FIFO_FULL_STATE) begin
-           next_state = FIFO_FULL_STATE;
-          if(!fifo_full) begin
-          next_state= LOAD_AFTER_FULL;
-          end
-end
-if(state == LOAD_AFTER_FULL) begin
-if (!parity_done && !low_pkt_valid)begin
- next_state = LOAD_DATA;
-end
- if (!parity_done && low_pkt_valid)begin
- next_state = LOAD_PARITY; 
-end
-if (parity_done) begin
-next_state = DECODE_ADDRESS;
-end
-end  
 
-if(state == LOAD_PARITY) begin
- next_state == LOAD_PARITY;
-end
+            DECODE_ADDRESS: begin
+                detect_add = 1;
+                if ((pkt_valid && (data_in == 2'b00) && fifo_empty_0) ||
+                    (pkt_valid && (data_in == 2'b01) && fifo_empty_1) ||
+                    (pkt_valid && (data_in == 2'b10) && fifo_empty_2)) begin
+                    next_state = LOAD_FIRST_DATA;
+                end else if ((pkt_valid && (data_in == 2'b00) && !fifo_empty_0) ||
+                             (pkt_valid && (data_in == 2'b01) && !fifo_empty_1) ||
+                             (pkt_valid && (data_in == 2'b10) && !fifo_empty_2)) begin
+                    next_state = WAIT_TILL_EMPTY;
+                end
+            end
 
-if(state == CHECK_PARITY_ERROR) begin
-if(fifo_full) begin
-next_state == FIFO_FULL_STATE;
-end
-if(!fifo_full) begin
-next_state == DECODE_ADDRESS;
-end
-end
+            WAIT_TILL_EMPTY: begin
+                busy = 1;
+                if ((fifo_empty_0 && (data_in == 2'b00)) ||
+                    (fifo_empty_1 && (data_in == 2'b01)) ||
+                    (fifo_empty_2 && (data_in == 2'b10))) begin
+                    next_state = LOAD_FIRST_DATA;
+                end
+            end
+
+            LOAD_FIRST_DATA: begin
+                lfd_state = 1;
+                write_enb_reg = 1;
+                busy = 1;
+                next_state = LOAD_DATA;
+            end
+
+            LOAD_DATA: begin
+                write_enb_reg = 1;
+                if (fifo_full) begin
+                    next_state = FIFO_FULL_STATE;
+                end else if (!fifo_full && !pkt_valid) begin
+                    next_state = LOAD_PARITY;
+                end
+            end
+
+            FIFO_FULL_STATE: begin
+                full_state = 1;
+                busy = 1;
+                if (!fifo_full) begin
+                    next_state = LOAD_AFTER_FULL;
+                end
+            end
+
+            LOAD_AFTER_FULL: begin
+                laf_state = 1;
+                write_enb_reg = 1;
+                busy = 1;
+                if (parity_done) begin
+                    next_state = DECODE_ADDRESS;
+                end else if (low_pkt_valid) begin
+                    next_state = LOAD_PARITY;
+                end else begin
+                    next_state = LOAD_DATA;
+                end
+            end
+
+            LOAD_PARITY: begin
+                write_enb_reg = 1;
+                busy = 1;
+                next_state = CHECK_PARITY_ERROR;
+            end
+
+            CHECK_PARITY_ERROR: begin
+                rst_int_reg = 1;
+                busy = 1;
+                if (fifo_full) begin
+                    next_state = FIFO_FULL_STATE;
+                end else begin
+                    next_state = DECODE_ADDRESS;
+                end
+            end
+
+            default: begin
+                next_state = IDLE; 
+            end
+        endcase
+    end
 endmodule
-                
